eb53a52e2b758ab933144bdfee73dbd95aefac92
Jan-Willem Baart <jwbaart@gmail.com>
2019-12-12 21:26:24 +0100

    feat: angularfire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { AngularFireModule } from "@angular/fire";
+import { environment } from "../environments/environment";
+import { AngularFirestoreModule } from "@angular/fire/firestore";
+import { AngularFireAuthModule } from "@angular/fire/auth";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase, "quotes"),
+    AngularFirestoreModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBVoDoVIUuedEiX-BhiHk80UyZFeFv81Eo",
+    authDomain: "quotes-553ed.firebaseapp.com",
+    databaseURL: "https://quotes-553ed.firebaseio.com",
+    projectId: "quotes-553ed",
+    storageBucket: "quotes-553ed.appspot.com",
+    messagingSenderId: "149174406899",
+    appId: "1:149174406899:web:628b1a056427ed4821b186",
+    measurementId: "G-D2L7910Y1T"
+  }
 };
 
 /*
