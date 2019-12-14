ca958aa44d331c8e1972278007bb7804a2b76102
Jan-Willem Baart <jwbaart@gmail.com>
2019-12-14 19:39:04 +0100

    fix(environment): firebase in prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBVoDoVIUuedEiX-BhiHk80UyZFeFv81Eo',
+    authDomain: 'quotes-553ed.firebaseapp.com',
+    databaseURL: 'https://quotes-553ed.firebaseio.com',
+    projectId: 'quotes-553ed',
+    storageBucket: 'quotes-553ed.appspot.com',
+    messagingSenderId: '149174406899',
+    appId: '1:149174406899:web:628b1a056427ed4821b186',
+    measurementId: 'G-D2L7910Y1T'
+  }
 };
