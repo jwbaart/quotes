faeae1f91f9e5467fa660a39624f91fc3ca717f5
Jan-Willem Baart <jwbaart@gmail.com>
2019-12-15 15:32:52 +0100

    feat: intro component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,17 @@ import { QuotesComponent } from './quotes/quotes.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { RegisterComponent } from './register/register.component';
+import { IntroComponent } from './intro/intro.component';
 
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'login',
+    redirectTo: 'intro',
     pathMatch: 'full'
   },
   { path: 'register', component: RegisterComponent },
-  { path: 'quotes', component: QuotesComponent }
+  { path: 'quotes', component: QuotesComponent },
+  { path: 'intro', component: IntroComponent }
 ];
 
 @NgModule({
