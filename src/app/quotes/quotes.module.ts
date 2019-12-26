38740ec1f2b71b875f1bd497fc9e4ffbd0216c75
Jan-Willem Baart <jwbaart@gmail.com>
2019-12-26 11:13:25 +0100

    fix: remove unused components

--- src/app/quotes/quotes.module.ts
+++ src/app/quotes/quotes.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { QuotesListComponent } from './quotes-list/quotes-list.component';
 import { QuotesComponent } from './quotes.component';
 import { QuoteTableComponent } from './quote-table/quote-table.component';
-import { QuoteTableRowComponent } from './quote-table-row/quote-table-row.component';
 import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
-  declarations: [QuotesListComponent, QuotesComponent, QuoteTableComponent, QuoteTableRowComponent],
+  declarations: [QuotesComponent, QuoteTableComponent],
   imports: [CommonModule, MatTableModule]
 })
 export class QuotesModule {}
