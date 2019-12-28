7ec9be39c9e83ffdbb5f1236cc48764bc0b37d4f
Jan-Willem Baart <jwbaart@gmail.com>
2019-12-28 22:17:27 +0100

    feat(navigation): add toolltip on logout

--- src/app/material-module.ts
+++ src/app/material-module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @NgModule({
-  exports: [MatToolbarModule, MatButtonModule, MatCardModule]
+  exports: [MatToolbarModule, MatButtonModule, MatCardModule, MatTooltipModule]
 })
 export class MaterialModule {}
