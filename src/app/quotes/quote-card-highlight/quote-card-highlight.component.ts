83b664f1914cef4cbbf412979c4722ee9925b53e
Jan-Willem Baart <jwbaart@gmail.com>
2020-01-01 22:34:05 +0100

    feat(quote): date on card

--- src/app/quotes/quote-card-highlight/quote-card-highlight.component.ts
+++ src/app/quotes/quote-card-highlight/quote-card-highlight.component.ts
@@ -1,13 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { QuoteCardComponent } from '../quote-card/quote-card.component';
+import { formatWithOptions } from 'date-fns/fp';
+import { nl } from 'date-fns/locale';
 
 @Component({
   selector: 'app-quote-card-highlight',
   templateUrl: './quote-card-highlight.component.html',
   styleUrls: ['./quote-card-highlight.component.scss']
 })
-export class QuoteCardHighlightComponent extends QuoteCardComponent {
+export class QuoteCardHighlightComponent extends QuoteCardComponent implements OnInit {
+  formattedDate;
+
   constructor() {
     super();
+  }
+
+  ngOnInit() {
+    if (this.quote && this.quote.datestamp) {
+      this.setFormattedDate(this.quote.datestamp);
+    }
+  }
+
+  setFormattedDate(datestamp) {
+    const dateToString = formatWithOptions({ locale: nl }, 'dd/MM/yyyy');
+
+    this.formattedDate = [datestamp.toDate()].map(dateToString);
   }
 }
