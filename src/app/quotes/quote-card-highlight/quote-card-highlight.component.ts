41f7ec0a734fd224df3598cbcfadce9a3192e4b6
Jan-Willem Baart <jwbaart@gmail.com>
2020-01-04 09:33:29 +0100

    fix(quote): simplified date

--- src/app/quotes/quote-card-highlight/quote-card-highlight.component.ts
+++ src/app/quotes/quote-card-highlight/quote-card-highlight.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { QuoteCardComponent } from '../quote-card/quote-card.component';
-import { formatWithOptions } from 'date-fns/fp';
-import { nl } from 'date-fns/locale';
 
 @Component({
   selector: 'app-quote-card-highlight',
@@ -15,15 +13,5 @@ export class QuoteCardHighlightComponent extends QuoteCardComponent implements O
     super();
   }
 
-  ngOnInit() {
-    if (this.quote && this.quote.datestamp) {
-      this.setFormattedDate(this.quote.datestamp);
-    }
-  }
-
-  setFormattedDate(datestamp) {
-    const dateToString = formatWithOptions({ locale: nl }, 'dd/MM/yyyy');
-
-    this.formattedDate = [datestamp].map(dateToString);
-  }
+  ngOnInit() {}
 }
