611db1d0b6c5c27e9f40798c7dd8b2fc1b52f5f7
Jan-Willem Baart <jwbaart@gmail.com>
2020-01-05 22:08:36 +0100

    Revert "build(e2e): typescript support"This reverts commit cd4b60d6b2b4406190a5983bc48b52f7ed3d66e5.

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,9 +1,17 @@
-const cypressTypeScriptPreprocessor = require('./cy-ts-preprocessor');
-const cypressFirebasePlugin = require('cypress-firebase').plugin;
+// ***********************************************************
+// This example plugins/index.js can be used to load plugins
+//
+// You can change the location of this file or turn off loading
+// the plugins file with the 'pluginsFile' configuration option.
+//
+// You can read more here:
+// https://on.cypress.io/plugins-guide
+// ***********************************************************
+
+// This function is called when a project is opened or re-opened (e.g. due to
+// the project's config changing)
 
 module.exports = (on, config) => {
-  on('file:preprocessor', cypressTypeScriptPreprocessor);
-
-  // Return extended config (with settings from .firebaserc)
-  return cypressFirebasePlugin(config);
+  // `on` is used to hook into various events Cypress emits
+  // `config` is the resolved Cypress config
 };
