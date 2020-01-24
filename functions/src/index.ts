e50a858ee1d8ae31bfc90492e31dbd2a68251263
Jan-Willem Baart <jwbaart@gmail.com>
2020-01-24 21:33:34 +0100

    ci(firebas): trigger

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,7 +3,7 @@ import * as admin from 'firebase-admin';
 
 admin.initializeApp();
 
-// Start writing Firebase Functions
+// Start writing Firebase Functions 
 // https://firebase.google.com/docs/functions/typescript
 export const helloWorld = functions.https.onRequest((request, response) => {
   response.send('Hello from Firebase index! again');
