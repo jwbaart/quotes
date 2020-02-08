c3f80f841756959e10c0ebe8cb8aac33ffdad4ab
Jan-Willem Baart <jwbaart@gmail.com>
2020-02-08 19:49:33 +0100

    feat(FirestoreCrudService): first commit

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,9 +2,10 @@ import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { AngularFireAuthGuard } from '@angular/fire/auth-guard';
 import { AuthService } from './services/auth.service';
 import { SnackbarService } from './services/snackbar.service';
+import { FirestoreCrudService } from './services/firestore-crud.service';
 
 @NgModule({
-  providers: [AngularFireAuthGuard, AuthService, SnackbarService]
+  providers: [AngularFireAuthGuard, AuthService, SnackbarService, FirestoreCrudService]
 })
 export class CoreModule {
   constructor(@Optional() @SkipSelf() core: CoreModule) {
