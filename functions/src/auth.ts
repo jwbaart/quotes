4408b3551e2de7fc7412871518427f3584e33b74
Jan-Willem Baart <jwbaart@gmail.com>
2020-03-02 21:22:56 +0100

    feat(roles): onCreate cloud function

--- functions/src/auth.ts
+++ functions/src/auth.ts
@@ -3,16 +3,27 @@ import * as admin from 'firebase-admin';
 
 const db = admin.firestore();
 
+export enum ROLE {
+  UNKNOWN = 'unknown'
+}
+
 export const createUserRecord = functions
   .region('europe-west1')
   .auth.user()
   .onCreate((user, context) => {
     const userRef = db.doc(`users/${user.uid}`);
-    return userRef.set({
-      name: user.displayName,
-      createdAt: context.timestamp,
-      photoURL: user.photoURL,
-      role: 'unknown',
-      uid: user.uid
-    });
+    const role = ROLE.UNKNOWN;
+    const uid = user.uid;
+
+    return userRef
+      .set({
+        name: user.displayName,
+        createdAt: context.timestamp,
+        photoURL: user.photoURL,
+        role,
+        uid
+      })
+      .then(() => admin.auth().setCustomUserClaims(uid, { role }))
+      .then(() => admin.auth().getUser(uid))
+      .then(() => null);
   });
