ff75612a5b85af6486c84b1824dd4529702eef64
Jan-Willem Baart <jwbaart@gmail.com>
2020-04-11 19:56:40 +0200

    test: updated cypress-firebase

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,17 +1,7 @@
-// ***********************************************************
-// This example plugins/index.js can be used to load plugins
-//
-// You can change the location of this file or turn off loading
-// the plugins file with the 'pluginsFile' configuration option.
-//
-// You can read more here:
-// https://on.cypress.io/plugins-guide
-// ***********************************************************
-
-// This function is called when a project is opened or re-opened (e.g. due to
-// the project's config changing)
+const admin = require('firebase-admin');
+const cypressFirebasePlugin = require('cypress-firebase').plugin;
 
 module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
+  // Pass on function, config, and admin instance. Returns extended config
+  return cypressFirebasePlugin(on, config, admin);
 };
