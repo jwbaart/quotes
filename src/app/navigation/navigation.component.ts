ee0fc6fbc68862c903ddc67ed9d44e88212a3204
Jan-Willem Baart <jwbaart@gmail.com>
2020-04-11 20:54:31 +0200

    ci: show env in header

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,15 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from '@app/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.scss']
 })
-export class NavigationComponent {
+export class NavigationComponent implements OnInit{
+  environmentTitle: string;
   constructor(public authService: AuthService) {}
 
+  ngOnInit() {
+    this.environmentTitle = environment.firebase.projectId;
+  }
+
   isLoggedIn() {
+    
     return this.authService.isLoggedIn();
   }
 
