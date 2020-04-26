25aa2f0a577aab446cf54e3c746cacbea97e4bdf
Jan-Willem Baart <jwbaart@gmail.com>
2020-04-26 13:47:57 +0200

    fix(navigation): remove env title

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,22 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '@app/core';
-import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.scss']
 })
-export class NavigationComponent implements OnInit{
-  environmentTitle: string;
+export class NavigationComponent implements OnInit {
   constructor(public authService: AuthService) {}
 
-  ngOnInit() {
-    this.environmentTitle = environment.firebase.projectId;
-  }
+  ngOnInit() {}
 
   isLoggedIn() {
-    
     return this.authService.isLoggedIn();
   }
 
