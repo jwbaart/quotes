bf33c238075d61b836eeb51d2d2c157e01948b77
Jan-Willem Baart <jwbaart@gmail.com>
2020-04-27 20:03:59 +0200

    Revert "ci: try global config vs seperate env params"
    This reverts commit dc6fe65df6418afc9a901d7e3caec5f6e25fc9e3.

--- config.index.ts
+++ config.index.ts
@@ -12,8 +12,7 @@ const firebaseEnvConfigFile = `module.exports = {
    databaseURL: '${process.env.FIREBASE_DATABASE_URL}',
    projectId: '${process.env.FIREBASE_PROJECT_ID}',
    storageBucket: '${process.env.FIREBASE_STORAGE_BUCKET}',
-   messagingSenderId: '${process.env.FIREBASE_MESSAGING_SENDER_ID}',
-   appId: '${process.env.FIREBASE_APP_ID}',
+   messagingSenderId: '${process.env.FIREBASE_MESSAGING_SENDER_ID}',    appId: '${process.env.FIREBASE_APP_ID}',
    measurementId: '${process.env.FIREBASE_MEASUREMENT_ID}'
 }`;
 
