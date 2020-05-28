36c1d99eb81b5a49f5e0d2476bb828e5eec273db
Jan-Willem Baart <jwbaart@gmail.com>
2020-05-28 21:14:36 +0200

    feat(users): set role for db user

--- functions/src/api/custom-claim/custom-claim.ts
+++ functions/src/api/custom-claim/custom-claim.ts
@@ -45,23 +45,39 @@ export async function customClaim(req: Request, res: Response): Promise<void> {
     return;
   }
 
-  setCustomUserClaims(uid, {
-    [claim]: true
-  })
-    .then(function() {
-      // Tell client to refresh token on user.
-      res.end(
-        JSON.stringify({
-          status: STATUS.SUCCESS
-        })
-      );
-    })
-    .catch(error => {
-      console.error('setCustomClaims - setCustomUserClaims failed', error);
-      res.end(JSON.stringify({ status: STATUS.FAILED }));
-    });
+  try {
+    await Promise.all([updateUser(uid, { role: claim }), setCustomUserClaims(uid, { [claim]: true })]);
+
+    res.end(
+      JSON.stringify({
+        status: STATUS.SUCCESS
+      })
+    );
+  } catch (error) {
+    console.error('setCustomClaims - setCustomUserClaims failed', error);
+    res.end(JSON.stringify({ status: STATUS.FAILED }));
+  }
 }
 
 const setCustomUserClaims = (uid: string, claims: { [key: string]: boolean }): Promise<void> => {
   return admin.auth().setCustomUserClaims(uid, claims);
+};
+
+// TODO: how to user angular types so we don't have to use copies here
+export interface User {
+  id: string;
+  name: string;
+  // createdAt: firebase.firestore.Timestamp; // TODO: how to get typing?
+  createdAt: string;
+  photoUrl: string;
+  role: ROLE;
+  uid: string;
+}
+
+const updateUser = (uid: string, userData: Partial<User>): Promise<FirebaseFirestore.WriteResult> => {
+  const db = admin.firestore();
+  return db
+    .collection('users')
+    .doc(uid)
+    .set(userData, { merge: true });
 };
