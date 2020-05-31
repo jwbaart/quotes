94020394f81356e797df2860060d257235dfe6bb
Jan-Willem Baart <jwbaart@gmail.com>
2020-05-31 08:47:01 +0200

    build(emultor): build env

--- config.index.ts
+++ config.index.ts
@@ -10,11 +10,13 @@ const firebaseEnvConfigFile = `module.exports = {
    apiKey: '${process.env.FIREBASE_API_KEY}',
    authDomain: '${process.env.FIREBASE_AUTH_DOMAIN}',
    databaseURL: '${process.env.FIREBASE_DATABASE_URL}',
+   functions: '${process.env.FIREBASE_FUNCTIONS}',
+   ssl: '${process.env.FIREBASE_SSL}',
    projectId: '${process.env.FIREBASE_PROJECT_ID}',
    storageBucket: '${process.env.FIREBASE_STORAGE_BUCKET}',
    messagingSenderId: '${process.env.FIREBASE_MESSAGING_SENDER_ID}',
    appId: '${process.env.FIREBASE_APP_ID}',
-   measurementId: '${process.env.FIREBASE_MEASUREMENT_ID}'
+   measurementId: '${process.env.FIREBASE_MEASUREMENT_ID}',
 }`;
 
 const envConfigFile = `import * as firebase from './../../firebase.config.js';
