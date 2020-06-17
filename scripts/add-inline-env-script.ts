a9bc10fb0b84f7724465562dfbd40ccf77468a70
Jan-Willem Baart <jwbaart@gmail.com>
2020-06-17 22:44:29 +0200

    build: hello world example

--- scripts/add-inline-env-script.ts
+++ scripts/add-inline-env-script.ts
@@ -1,2 +1,10 @@
-import { writeFile } from 'fs';
+import { readFileSync } from 'fs';
 import { JSDOM } from 'jsdom';
+
+const htmlPath = 'scripts/index.html';
+
+const htmlFile = readFileSync(htmlPath, 'utf8');
+
+const dom = new JSDOM(htmlFile);
+console.log(dom.window.document.querySelector("p").textContent); // "Hello world"
+
