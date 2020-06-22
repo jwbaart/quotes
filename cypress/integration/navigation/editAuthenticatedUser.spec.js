d81bee970d6f4c39ecceb3fdd303a547740e0faa
Jan-Willem Baart <jwbaart@gmail.com>
2020-06-22 19:47:35 +0200

    test(edit-user): edit authenticated user actions

---
+++ cypress/integration/navigation/editAuthenticatedUser.spec.js
@@ -0,0 +1,35 @@
+describe('Edit authenticatd user', () => {
+  const editAuthenticatdUser = require('./editAuthenticatdUser.page');
+
+  beforeEach(() => {
+    cy.login();
+    cy.visit('/quotes');
+  });
+
+  describe('User details', () => {
+    it('should show user details dialog', () => {
+      editAuthenticatdUser.openDialog();
+
+      editAuthenticatdUser.getDialog().should('be.visible');
+    });
+
+    it('should hide dialog on close', () => {
+      editAuthenticatdUser.openDialog();
+      editAuthenticatdUser.closeDialog();
+
+      editAuthenticatdUser.getDialog().should('not.exist');
+    });
+
+    it('should show user role', () => {
+      editAuthenticatdUser.openDialog();
+      editAuthenticatdUser
+        .getRole()
+        .invoke('text')
+        .then(roleText => {
+          const hasRoleText = roleText && !!roleText.length;
+
+          expect(hasRoleText).to.be.true;
+        });
+    });
+  });
+});
