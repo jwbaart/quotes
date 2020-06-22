d53828fd00af704bc9ce1704f5fdcde45f5e9687
Jan-Willem Baart <jwbaart@gmail.com>
2020-06-22 20:06:18 +0200

    test(edit-user): edit name

--- cypress/integration/navigation/editAuthenticatdUser.page.js
+++ cypress/integration/navigation/editAuthenticatdUser.page.js
@@ -4,6 +4,8 @@ class EditAuthenticatdUser {
   selectors = {
     dialog: '[data-test=edit-authenticated-user]',
     role: '[data-test=edit-authenticated-user-role]',
+    name: '[data-test=edit-authenticated-user-name]',
+    nameEditable: '[data-test=edit-authenticated-user-name-editable]',
     closeButton: '[data-test=edit-authenticated-user-close]'
   };
   getDialog() {
@@ -11,6 +13,12 @@ class EditAuthenticatdUser {
   }
   getRole() {
     return cy.get(this.selectors.role);
+  }
+  getName() {
+    return cy.get(this.selectors.name);
+  }
+  getNameEditable() {
+    return cy.get(this.selectors.nameEditable);
   }
   closeDialog() {
     return cy.get(this.selectors.closeButton).click();
