8b8097eb1fb4bb1f9725d3359a3638bb79264b46
Jan-Willem Baart <jwbaart@gmail.com>
2021-11-02 18:55:26 +0100

    build: cypress and firebase upgrade

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,7 +1,12 @@
 const admin = require('firebase-admin');
 const cypressFirebasePlugin = require('cypress-firebase').plugin;
+const wp = require('@cypress/webpack-preprocessor');
 
 module.exports = (on, config) => {
+  const options = {
+    webpackOptions: require('../webpack.config.js')
+  };
+  on('file:preprocessor', wp(options));
   // Pass on function, config, and admin instance. Returns extended config
   return cypressFirebasePlugin(on, config, admin);
 };
